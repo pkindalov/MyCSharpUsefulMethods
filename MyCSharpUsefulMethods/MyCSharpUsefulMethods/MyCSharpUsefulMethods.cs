a5e869eb406ec8653ddbc98ca0a0cba5c03d35ad
pkindalov <panayotkindalov@gmail.com>
2016-09-15 22:32:03 +0300

    Rename project name. Added first method static string printCharsString which return a string like a result.

---
+++ MyCSharpUsefulMethods/MyCSharpUsefulMethods/MyCSharpUsefulMethods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharpUsefulMethods
+{
+    class MyCSharpUsefulMethods
+    {
+
+        //write chars on console with parameter in string format
+        static string printCharsString(string str, int count)
+        {
+            string result = null;
+
+            for (int i = 0; i < count; i++)
+            {
+                result += str + " ";
+            }
+
+            return result;
+
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(printCharsString("Some text", 3));
+
+            string someText = printCharsString("Some text2", 5);
+            Console.WriteLine(someText);
+        }
+    }
+}
